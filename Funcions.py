6d585bef50570a24c4c2f2c51c39ba46bb7e2f75
jordicaritg <caritg40@gmail.com>
2023-08-30 17:17:47 +0200

    Canvis varis

--- Funcions.py
+++ Funcions.py
@@ -3,3 +3,25 @@
 def my_function ():
     print("Això és una funció")
 
+my_function ()
+my_function ()
+my_function ()
+
+def sum_two_values (first_value, second_value):
+    print(first_value + second_value)
+
+sum_two_values(5,7)
+sum_two_values(58489,78431)
+sum_two_values("5","7")
+sum_two_values(4.3,5)
+
+def sum_two_values_with_return (first_value, second_value):
+    return first_value + second_value #return és per guardar-me el resultat en una variable
+
+my_result = sum_two_values_with_return(10, 5)
+print(my_result)
+
+def print_name (name, surname):
+    print(f"{name} {surname}")
+
+print_name(surname = "Caritg", name = "Jordi")
